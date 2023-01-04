6fa3841cb2fa440823857eb985aa6fda2862b0e5
Piter Rivero <p.rivero@crif.com>
2023-01-04 12:34:51 +0100

    added open api swagger

--- band-service/src/main/java/com/band/service/BandServiceApplication.java
+++ band-service/src/main/java/com/band/service/BandServiceApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @EnableFeignClients
 @EnableEurekaClient
+@OpenAPIDefinition
 @SpringBootApplication
 public class BandServiceApplication {
 
