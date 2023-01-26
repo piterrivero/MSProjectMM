c072aa6f5986b28c03d1d3ba5b067dd8d9705740
Piter Rivero <p.rivero@crif.com>
2023-01-26 21:20:40 +0100

    fixed kafka messages

--- notification-service/src/main/java/com/notification/service/entity/Notification.java
+++ notification-service/src/main/java/com/notification/service/entity/Notification.java
@@ -6,14 +6,18 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Document(collection = "notification")
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification {
 	
 	@Transient
